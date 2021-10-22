fd4c30b4f5a21f718275b27f928bd8e69bf107a0
Marian Hahne <marian.hahne@outlook.com>
2021-10-22 17:57:49 +0200

    Add global tsconfig and .eslintrc

---
+++ api/app.ts
@@ -0,0 +1,14 @@
+interface App {
+  host: string;
+  port: number;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const app: App = {
+  host: 'localhost',
+  port: 8_080
+};
+
+// Const print = function (value: unknown): void {
+//
+// }
