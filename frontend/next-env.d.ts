7339c254cde43acc8a3181a3b709b66515da4f93
Marian Hahne <marian.hahne@outlook.com>
2021-10-29 23:08:20 +0200

    Add Nuxt.js

--- frontend/next-env.d.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable eslint-comments/disable-enable-pair */
-/* eslint-disable spaced-comment */
-/// <reference types="next" />
-/// <reference types="next/types/global" />
-/// <reference types="next/image-types/global" />
-
-// NOTE: This file should not be edited
-// see https://nextjs.org/docs/basic-features/typescript for more information.
