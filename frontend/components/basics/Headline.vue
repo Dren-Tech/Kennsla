84b4e3735a1c695b49b125086ce797416f50f4dd
Marian Hahne <marian@marianhahne.de>
2022-03-18 17:50:38 +0100

    Remove old code

--- frontend/components/basics/Headline.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <h2 class="text-3xl font-medium">
-        <slot></slot>
-    </h2>
-</template>
--- frontend/layouts/backend.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="container mx-auto backend">
-    HEADER
-    <Nuxt />
-    FOOTER
-  </div>
-</template>
