595f95dd55840cd7edf6026e67f79c95ddb35e66
Marian Hahne <marian@marianhahne.de>
2022-04-09 20:50:46 +0200

    Add mechanism to check api key

--- server/http/middleware/ApiKeyMiddleware.go
+++ server/http/middleware/ApiKeyMiddleware.go
@@ -1,12 +1,15 @@
 package middleware
 
 import (
+	"server/domain/repository"
+
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
 func GetKeyAuthMiddleware() echo.MiddlewareFunc {
 	return middleware.KeyAuth(func(key string, c echo.Context) (bool, error) {
-		return key == "TEST", nil
+		apiKey := repository.GetApiKey(key)
+		return apiKey.Key != "", nil
 	})
 }
