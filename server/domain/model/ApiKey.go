ee57b5a66dfcd026c7b194e9d77013b68f847e59
Marian Hahne <marian@marianhahne.de>
2022-04-09 20:31:17 +0200

    Add model ApiKey

---
+++ server/domain/model/ApiKey.go
@@ -0,0 +1,25 @@
+package model
+
+import (
+	"math/rand"
+
+	"gorm.io/gorm"
+)
+
+const API_KEY_LENGTH = 12
+
+type ApiKey struct {
+	gorm.Model
+	Key      string
+	IsActive bool
+}
+
+func (a *ApiKey) GenerateKey() {
+	var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
+	s := make([]rune, API_KEY_LENGTH)
+	for i := range s {
+		s[i] = letters[rand.Intn(len(letters))]
+	}
+	a.Key = string(s)
+}
